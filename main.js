b5f1c5a43193d95b344870093712aaf998eb7846
Bill Zhou <zhou.xu@husky.neu.edu>
2022-10-04 14:47:36 -0400

    Almost finished

--- main.js
+++ main.js
@@ -2,14 +2,14 @@
 
 function circleClicked(circle) {
     clicked = document.getElementById(circle.target.id);
-    // let textDiv = document.getElementById("last-selected");
+    let textDiv = document.getElementById("last-selected");
 	if (clicked.classList.contains("selected")) {
 		clicked.classList.remove("selected");
 	}
 	else {
 		clicked.classList.add("selected");
 	}
-	// textDiv.innerHTML = "Last point clicked: " + circle.target.id;
+	textDiv.innerHTML = "Last point clicked: " + circle.target.id;
 
 }
 
@@ -21,14 +21,15 @@ function addPoint() {
     cy = Y.options[Y.selectedIndex].text;
 
     frame = document.getElementById('frame');
-    frame.insertAdjacentHTML("beforeend", "<circle id='(" + cx + "," + cy + ")' cx='" + (40*cx + 50) + "' cy='" + (440 - 40*cy) + "' r='10'/>");
+    frame.insertAdjacentHTML("afterbegin", "<circle id='(" + cx + "," + cy + ")' cx='" + (40*cx + 50) + "' cy='" + (440 - 40*cy) + "' r='10'/>");
 
 }
 
 
 button = document.getElementById('button');
 button.addEventListener("click", addPoint);
-let circles = document.getElementsByTagName('circle')
+let circles = document.getElementsByTagName('circle');
+console.log(circles)
 for (let i = 0; i < circles.length; i++) {
 	circles[i].addEventListener("click", circleClicked);
 }
