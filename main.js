ed3fd7929a1e160e2b3a79365716f66c807b297c
Bill Zhou <zhou.xu@husky.neu.edu>
2022-10-04 14:29:35 -0400

    Updated html and js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let circles = document.getElementsByTagName('circle')
+
 
 function circleClicked(circle) {
     clicked = document.getElementById(circle.target.id);
@@ -13,6 +13,22 @@ function circleClicked(circle) {
 
 }
 
+function addPoint() {
+    X = document.getElementById('x-value');
+    cx = X.options[X.selectedIndex].text;
+
+    Y = document.getElementById('y-value');
+    cy = Y.options[Y.selectedIndex].text;
+
+    frame = document.getElementById('frame');
+    frame.insertAdjacentHTML("beforeend", "<circle id='(" + cx + "," + cy + ")' cx='" + (40*cx + 50) + "' cy='" + (440 - 40*cy) + "' r='10'/>");
+
+}
+
+
+button = document.getElementById('button');
+button.addEventListener("click", addPoint);
+let circles = document.getElementsByTagName('circle')
 for (let i = 0; i < circles.length; i++) {
 	circles[i].addEventListener("click", circleClicked);
 }
